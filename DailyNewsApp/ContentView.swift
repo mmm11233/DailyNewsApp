922a09ad1ff1875b598f023e10546491575433ac
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-12-20 13:05:26 +0400

    Initial Commit

---
+++ DailyNewsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyNewsApp
+//
+//  Created by Mariam Joglidze on 20.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyNewsApp/DailyNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNewsAppApp.swift
+//  DailyNewsApp
+//
+//  Created by Mariam Joglidze on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct DailyNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
